e125f9b9a9f7ba72242233ac714d8ade748f3463
nayeemmohamed <nayeemmohamed2409@gmail.com>
2021-04-17 16:09:46 +0500

    server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var moment = require('moment');
+var app=express()
+
+app.use(express.static(__dirname + '/public'));
+
+var printMessage  = function(message){
+    console.log('[Server] @' + moment().format()+' '+message)
+}
+
+var port = 3000;
+app.listen(port);
+printMessage("Listening on port: "+port);
+
